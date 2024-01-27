8095eb570cc9b3840ff3dfe12a4b50715e647a32
LiuShuoJiang <shuojiang@gatech.edu>
2024-01-27 13:28:09 -0500

    Add signal handler

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,7 +1,20 @@
 #include "ChatServer.h"
+#include "ChatService.h"
 #include <iostream>
+#include <csignal>
+
+// Reset user's status information
+// at the end of terminating server with CTRL+C
+void resetHandler(int) {
+    ChatService::instance()->reset();
+    exit(0);
+}
 
 int main() {
+
+
+    signal(SIGINT, resetHandler);
+
     EventLoop loop;
     InetAddress addr("127.0.0.1", 6000);
 
