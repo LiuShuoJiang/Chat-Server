e1d7f0927e946370b47d6caa53b42e8a1db03dd3
LiuShuoJiang <shuojiang@gatech.edu>
2024-01-27 21:50:37 -0500

    Update command line argument formats

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -2,6 +2,7 @@
 #include "ChatService.h"
 #include <iostream>
 #include <csignal>
+#include <cstdlib>
 
 // Reset user's status information
 // at the end of terminating server with CTRL+C
@@ -10,13 +11,19 @@ void resetHandler(int) {
     exit(0);
 }
 
-int main() {
+int main(int argc, char **argv) {
+    if (argc < 3) {
+        std::cerr << "Command is invalid! Example: ./ChatServer 127.0.0.1 6000" << std::endl;
+        exit(-1);
+    }
 
+    char *ip = argv[1];
+    uint16_t port = atoi(argv[2]);
 
     signal(SIGINT, resetHandler);
 
     EventLoop loop;
-    InetAddress addr("127.0.0.1", 6000);
+    InetAddress addr(ip, port);
 
     ChatServer server(&loop, addr, "Chat Server");
 
